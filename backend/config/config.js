fda06b10ca39208b77cc9daf0af37a86939216fe
Pavel Sobolev <pavelsobolev1991@gmail.com>
2022-08-18 20:37:29 +0300

    added YandexCard

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,14 +2,18 @@ const express = require('express');
 const morgan = require('morgan');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const sessionConfig = require('./sessionConfig');
 const ssr = require('../middleware/ssr');
 
 module.exports = function config(app) {
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({
+    extended: true
+  }));
   app.use(express.json());
   app.use(morgan('dev'));
+  app.use(express.static(path.join(__dirname, '../../frontend')));
   app.use(express.static('public'));
   app.use(cookieParser());
   app.use(session(sessionConfig));
