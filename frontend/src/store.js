16c137695c7d0c82c91203cc776b29834d56d8f5
Anton Gorozhankin <obrv.igl@gmail.com>
2022-08-19 20:03:51 +0300

    Weather API Release 2.1

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,12 +3,14 @@ import {
   configureStore
 } from '@reduxjs/toolkit';
 import navBarSlice from './features/main/auth.js';
-// import categorySlice from './Category/category';
+// import categorySlice from './Category/category.js';
+import weatherSlice from './features/weather/weather.js';
 
 const store = configureStore({
   reducer: {
     auth: navBarSlice,
     // category: categorySlice,
+    weather: weatherSlice,
   }
 });
 
