5f73b95453d1b55542e6979e4ddac1e788dd9b24
Anton Gorozhankin <obrv.igl@gmail.com>
2022-08-20 21:57:15 +0300

    Weather Release 2.5

--- frontend/src/features/weather/Weather.jsx
+++ frontend/src/features/weather/Weather.jsx
@@ -4,15 +4,12 @@ import { selectorCategory, categoryThunk } from '../Category/category';
 import { getCoordinate, selectorWeather } from './weather';
 import './Weather.css';
 import Category from '../Category/Category.jsx';
+import { placeThunk, selectorPlaces } from '../Category/places';
 
 function Weather() {
 const dispatch = useDispatch();
 const weatherSelector = useSelector(selectorWeather);
-const category = useSelector(selectorCategory);
-// eslint-disable-next-line no-console
-console.log(category, 'categoryyyyyyyyyyyyy');
-
-console.log(weatherSelector, 'weather');
+const places = useSelector(selectorPlaces);
 
 const searchLocation = async (event) => {
   dispatch(getCoordinate({ lat: 59.57, lon: 30.18 }));
@@ -20,23 +17,26 @@ const searchLocation = async (event) => {
 
 useEffect(() => {
   searchLocation();
-  dispatch(categoryThunk(2));
-  console.log('useEffect');
-}, []);
-
+  dispatch(categoryThunk(1));
+  dispatch(placeThunk(6));
+  console.log('места', places);
+}, [dispatch]);
+console.log(weatherSelector, 'вот он');
 return (
   <div>
     <div className="weather-days">
       {weatherSelector && weatherSelector.map((day) => (
-        <div className="weather-day" key={day.id}>
-          <h3>{day.dt_txt.slice(5, 10).split('-').reverse().join('.')}</h3>
-          <h4>
+        <div className="weather-day" key={day.dt}>
+          <div>
+            <h4>{day.dt_txt.slice(5, 10).split('-').reverse().join('.')}</h4>
+          </div>
+          <div>
             {`${day.main.temp.toFixed(1)} °C`}
             <img className="weather-img" src={`/images/categories/weather/${day.weather[0].main.toLowerCase()}.png`} alt="img" />
-            <h4>
+            <p>
               {`Ветер ${day.wind.speed.toFixed(1)} м/с`}
-            </h4>
-          </h4>
+            </p>
+          </div>
         </div>
       )
       )}
@@ -47,63 +47,3 @@ return (
 }
 
 export default Weather;
-
-// import React, { useState } from 'react';
-// import './Weather.css';
-
-// function App() {
-//   const [data, setData] = useState();
-//   // const [input, setCity] = useState({});
-//   const [location, setLocation] = useState([]);
-//   const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=50.09&lon=30.09&units=metric&&appid=7aa47db807150d867450e03bfa107b9e';
-
-//   const searchLocation = async (event) => {
-//     if (event.key === 'Enter') {
-//       const response = await fetch(url);
-//       const datas = await response.json();
-//       const arrDays = [];
-//       for (let i = 1; i < datas.list.length; i += 8) {
-//           arrDays.push(datas.list[i]);
-//         }
-//         setData((prev) => {
-//           prev = arrDays;
-//           return prev;
-//         });
-//         // console.log(arrDays[0]);
-//         setLocation('');
-//       }
-//   };
-
-// return (
-//   <div>
-//     <div className="search">
-//       <input
-//         type="text"
-//         value={location}
-//         onChange={(event) => setLocation(event.target.value)}
-//         onKeyPress={searchLocation}
-//         placeholder="Ваш город"
-//       />
-//     </div>
-//     <div className="weather-days">
-//       {data && data.map((day) => (
-//         <div className="weather-day" key={day.id}>
-//           <h3>{day.dt_txt.slice(5, 10).split('-').reverse().join('.')}</h3>
-//           <h4>
-//             {`${day.main.temp.toFixed(1)} °C`}
-// eslint-disable-next-line max-len
-//             <img className="weather-img" src={`/images/categories/weather/${day.weather[0].main.toLowerCase()}.png`} alt="img" />
-//             <h4>
-//               {`Ветер ${day.wind.speed} м/с`}
-//             </h4>
-//           </h4>
-//         </div>
-//       )
-//       )}
-//     </div>
-
-//   </div>
-// );
-// }
-
-// export default App;
