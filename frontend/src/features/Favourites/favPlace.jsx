295884b2bc2a4e35a977ea67e3b659e761c2d36e
Pavel Sobolev <pavelsobolev1991@gmail.com>
2022-08-22 18:57:53 +0300

    favourites v.1.3

--- frontend/src/features/Favourites/favPlace.jsx
+++ frontend/src/features/Favourites/favPlace.jsx
@@ -5,7 +5,7 @@ export default function FavPlaces({ favPlace }) {
   // console.log(favPlace);
   return (
     <div>
-      <Card className="favDescription" id="favPhoto" />
+      <Card className="favDescription" id="favPhoto"><img src={`${favPlace['Photos.title']}`} alt="" /></Card>
       <Card className="favDescription" id="favDescription">
         {favPlace.title}
       </Card>
