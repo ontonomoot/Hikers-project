08d3ad0ce669cf062423aaccfa112dd4fb9c6e27
Ekaterina Ekimova <psich.kawai@gmail.com>
2022-08-23 19:14:24 +0300

    fixed console warnings and errors

--- frontend/src/features/placePage/reviews/ReviewList.jsx
+++ frontend/src/features/placePage/reviews/ReviewList.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Card, Text, User, Divider } from '@geist-ui/core';
 import Star from '@geist-ui/icons/star';
+import { v4 as uuidv4 } from 'uuid';
 import { selectorUserSession } from '../../main/auth';
 import { loadReview, selectReview, selectReviewPhoto } from './review';
 import ReviewForm from './ReviewForm';
@@ -24,7 +25,7 @@ function ReviewList() {
       {user && <ReviewForm />}
       <div className="reviews-list-container">
         {review.map((el) => (
-          <Card width="700px">
+          <Card key={uuidv4()} width="700px">
             <User src={el['User.ava']} name={el['User.user_name']} />
             <Text>{el.title}</Text>
             <Text>{el.description}</Text>
@@ -35,7 +36,7 @@ function ReviewList() {
                 .filter((photo) => photo.review_id === el.id)
                 .map((ph) =>
                   // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-                  <img className="review-img" tabIndex="0" src={ph.title} alt="some" />
+                  <img key={uuidv4()} className="review-img" tabIndex="0" src={ph.title} alt="some" />
                 )}
             </div>
             <Card.Footer>
