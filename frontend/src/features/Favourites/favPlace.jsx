d9816f0384032485361dd123dbffb9c76c7c2790
Pavel Sobolev <pavelsobolev1991@gmail.com>
2022-08-23 18:44:49 +0300

    favourites v.1.9

--- frontend/src/features/Favourites/favPlace.jsx
+++ frontend/src/features/Favourites/favPlace.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/jsx-no-bind */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
-import { Card, Text, Divider, Button } from '@geist-ui/core';
+import { Card, Text, Divider } from '@geist-ui/core';
 import Star from '@geist-ui/icons/star';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 import {
   deleteFavPlaceThunk, selectorFavourites
   // eslint-disable-next-line import/no-useless-path-segments
@@ -37,13 +39,13 @@ export default function FavPlaces({ favPlace }) {
           <p className="favPlaceTitle" onClick={() => navigate(`/categories/${favPlace.category_id}/places/${favPlace.id}`)}>
             {favPlace && favPlace.title}
           </p>
-          <div className="close" id={favPlace.id} onClick={handleClick} />
         </div>
         <p className="favPlaceDesc">{favPlace && `${favPlace.description.slice(100)}......`}</p>
         <p className="favPlaceDesc">
           Рейтинг: {favPlace && favPlace.rating ? Array.from({ length: favPlace.rating }, (_, i) => <Star key={i} color="orange" size={22} />) : <p>У места нет отзывов</p>}
         </p>
-        <button type="button" className="favPlaceBtn">Собраться!</button>
+        <Button variant="outline-success" className="favPlaceBtn">Собраться</Button>
+        <Button variant="outline-secondary" id={favPlace.id} onClick={handleClick} className="delPlaceBtn">Удалить</Button>
       </Card>
     </div>
   );
