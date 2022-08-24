96ee7633bc8f0645fe64bd1fea1e4ad3d6e246b0
serejqka <serejaseed@yandex.ru>
2022-08-24 22:44:19 +0300

    footer done

--- frontend/src/features/main/footer/Footer.jsx
+++ frontend/src/features/main/footer/Footer.jsx
@@ -1,13 +1,15 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { selectorCategories } from '../mainPage';
 
 import css from './Footer.module.css';
 
 export default function FooterMain() {
   const categories = useSelector(selectorCategories);
-
-  console.log(categories);
+  const navigate = useNavigate();
 
   if (!categories) return <div>Loading...</div>;
 
@@ -17,24 +19,35 @@ export default function FooterMain() {
         <div className={css.mainMarques}>
           <div className={css.mainMarq}>
             {categories && categories.map((category) => (
-              <div
-                // style={{ backgroundImage: `url(/images/photo/${category.photo})` }}
-                className={`${css.one} ${css.images}${category.id}`}
-              >
-                {category.title}
+              <div className={css.one}>
+                <img
+                  style={{ overflow: 'hidden' }}
+                  src={`/images/footer/${category.photo}`}
+                  className={css.oneImg}
+                  alt=""
+                />
+                <p
+                  onClick={() => navigate(`/categories/${category.id}`)}
+                  className={css.oneP}
+                >{category.title}
+                </p>
               </div>
             ))}
           </div>
           <div className={css.mainMarq}>
             {categories && categories.map((category) => (
-              <div
-                className={css.one}
-                style={{
-                  // backgroundImage: `url(/images/photo/${category.photo})`,
-                  padding: '20px'
-                }}
-              >
-                {category.title}
+              <div className={css.one}>
+                <img
+                  style={{ overflow: 'hidden' }}
+                  src={`/images/footer/${category.photo}`}
+                  className={css.oneImg}
+                  alt=""
+                />
+                <p
+                  onClick={() => navigate(`/categories/${category.id}`)}
+                  className={css.oneP}
+                >{category.title}
+                </p>
               </div>
             ))}
           </div>
