d2623c64ddd144cbb1d8687fe8a4a85f8d171d07
Ekaterina Ekimova <psich.kawai@gmail.com>
2022-08-24 15:05:22 +0300

    added slice & route draft for loading tasks

---
+++ backend/routes/api/todoRouter.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+// router
+//   .get('/place/:id/tasks', (req, res) => {
+//     const { id } = req.params;
+//     const userId = req.session.user.id;
+//     try {
+
+//     } catch (err) {}
+//   })
+
+module.exports = router;
---
+++ frontend/src/features/todoList/todoSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  tasks: []
+};
+
+// экспорт санок для загрузки списка задач
+export const loadTasks = createAsyncThunk(
+  'todo/loadTasks',
+  async (placeId) => {
+    const response = await fetch(`/api/place/${placeId}/tasks}`);
+    const data = await response.json();
+    return data;
+  }
+);
+
+const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadTasks.fulfilled, (state, action) => {
+        state.tasks = action.payload;
+      });
+  }
+});
+
+// экспорт функции редьюсера
+export default todoSlice.reducer;
